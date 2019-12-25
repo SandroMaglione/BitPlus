270337bf355a5121fc2ffab4f1054c29530dd43d
Sandro Maglione <lass.maglio@gmail.com>
2019-12-25 20:26:38 +0100

    Playing with GestureDetector

--- client/lib/app/presentation/widgets/habit_tile.dart
+++ client/lib/app/presentation/widgets/habit_tile.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
-class HabitTile extends StatelessWidget {
+class HabitTile extends StatefulWidget {
   final HabitApi habit;
 
   const HabitTile({
@@ -14,69 +14,107 @@ class HabitTile extends StatelessWidget {
   });
 
   @override
+  _HabitTileState createState() => _HabitTileState();
+}
+
+class _HabitTileState extends State<HabitTile> {
+  double startPress = 0;
+  double currentPress = 0;
+  bool isMoving = false;
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 20.0,
       ),
-      child: Card(
-        color: habit.checked ? Color(habit.color) : null,
-        margin: const EdgeInsets.only(
-          bottom: 2.0,
-          top: 2.0,
-        ),
-        elevation: 0.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 12.0,
-            vertical: 6.0,
+      child: GestureDetector(
+        onHorizontalDragUpdate: (details) {
+          setState(() {
+            currentPress = details.localPosition.dx;
+          });
+        },
+        onHorizontalDragStart: (details) {
+          setState(() {
+            startPress = details.localPosition.dx;
+            currentPress = details.localPosition.dx;
+            isMoving = true;
+          });
+        },
+        onHorizontalDragEnd: (_) {
+          setState(() {
+            isMoving = false;
+          });
+        },
+        child: Transform.translate(
+          offset: Offset(
+            isMoving ? currentPress - startPress : 0,
+            0,
           ),
-          child: ListTile(
-            title: Text('${habit.name}'),
-            subtitle: Text('${habit.habitID}'),
-            leading: IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  Router.updateHabitScreen,
-                  arguments: habit,
-                );
-              },
-              icon: Icon(Icons.update),
-            ),
-            trailing: InkWell(
-              onTap: () {
-                if (habit.checked) {
-                  BlocProvider.of<HabitListBloc>(context).add(
-                    HabitListEvent.habitListUncheck(
-                      habitID: habit.habitID,
-                    ),
-                  );
-                } else {
-                  BlocProvider.of<HabitListBloc>(context).add(
-                    HabitListEvent.habitListCheck(
-                      habitID: habit.habitID,
-                    ),
-                  );
-                }
-              },
-              child: !habit.checked
-                  ? CircleAvatar(
-                      child: Icon(
-                        Icons.check,
-                        color: WHITE,
-                      ),
-                      backgroundColor: Color(habit.color),
-                    )
-                  : CircleAvatar(
-                      child: Icon(
-                        Icons.check,
-                        color: WHITE,
-                      ),
-                      backgroundColor: Color(habit.color),
-                    ),
+          child: Transform.scale(
+            scale: isMoving ? 1.05 : 1,
+            child: Card(
+              color: widget.habit.checked ? Color(widget.habit.color) : null,
+              margin: const EdgeInsets.only(
+                bottom: 6.0,
+                top: 6.0,
+              ),
+              elevation: isMoving ? 36.0 : 0.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 12.0,
+                  vertical: 6.0,
+                ),
+                child: ListTile(
+                  title: Text('${widget.habit.name}'),
+                  subtitle: Text('${widget.habit.habitID}'),
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(
+                        Router.updateHabitScreen,
+                        arguments: widget.habit,
+                      );
+                    },
+                    icon: Icon(Icons.update),
+                  ),
+                  trailing: InkWell(
+                    onTap: () {
+                      if (widget.habit.checked) {
+                        BlocProvider.of<HabitListBloc>(context).add(
+                          HabitListEvent.habitListUncheck(
+                            habitID: widget.habit.habitID,
+                          ),
+                        );
+                      } else {
+                        BlocProvider.of<HabitListBloc>(context).add(
+                          HabitListEvent.habitListCheck(
+                            habitID: widget.habit.habitID,
+                          ),
+                        );
+                      }
+                    },
+                    child: widget.habit.checked
+                        ? CircleAvatar(
+                            child: Icon(
+                              Icons.check,
+                              color: WHITE,
+                            ),
+                            backgroundColor: Color(widget.habit.color),
+                          )
+                        : CircleAvatar(
+                            child: Icon(
+                              Icons.error_outline,
+                              color: Color(widget.habit.color),
+                            ),
+                            backgroundColor: SCAFFOLD_COLOR,
+                            foregroundColor: Color(widget.habit.color),
+                          ),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
